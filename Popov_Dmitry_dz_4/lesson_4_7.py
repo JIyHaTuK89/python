950cb90bacc0edf3d1a3d433e49c307a58b3ed9c
Dmitry <Somnum89@yandex.ru>
2021-06-08 10:49:10 +0300

    7th task edited ones more

--- Popov_Dmitry_dz_4/lesson_4_7.py
+++ Popov_Dmitry_dz_4/lesson_4_7.py
@@ -3,10 +3,11 @@ def fact(n):
     for number in range(1, n + 1):
         yield number
         result *= number
+        # yield result
 
 if __name__ == '__main__':
     print(fact(4)) # проверка возвращаемого объекта
     # здесь не понял, если только первые n числа то есть без последнего? Тогда там if el != 4: дописать нужно
-    # либо в самой функции добавить if number != n
+    # либо в самой функции добавить if number != n. А если промежуточные значения, то yield result
     for el in fact(4):
         print(el)
